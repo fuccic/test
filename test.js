7fd607fa1eb068e0bf2393b6b6a6f8362c4b1a13
Conor Fucci <cfucci@Conors-MacBook-Pro.local>
2015-11-17 09:40:33 -0500

    test4

--- test.js
+++ test.js
@@ -1,18 +1,1 @@
-arr = ["lkh", "ougiyfdgfx", "vngfb"];
-
-for (var i = 0; i < arr.length; i++) {
-	console.log(arr[i])
-};
-
-
-arr_goops = [6, 986, 466, 420, 69, 666]
-
-for (var i = 0; i < arr_goops.length; i++) {
-	console.log(arr_goops[i]);
-};
-
-arr_beps = [0, 0, 0, 0];
-
-for (var i = 0; i < arr_beps.length; i++) {
-	console_log(arr_beps[i])
-};
+LAJBSDKLFAHDSOPFGIDRHJFPIOAXLKHDJVAPDFIOLKGH>ADPIOFKLHDAPFILKLSJDFLAISKDLADKHFLDSIKFNASDILFKNASDPLVIKHNVPINADSFLIj

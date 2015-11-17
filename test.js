ba882f66e0ee71c99ce39b4beee29c17cb7844c3
Conor Fucci <cfucci@Conors-MacBook-Pro.local>
2015-11-17 09:30:18 -0500

    test2

--- test.js
+++ test.js
@@ -3,3 +3,10 @@ arr = ["lkh", "ougiyfdgfx", "vngfb"];
 for (var i = 0; i < arr.length; i++) {
 	console.log(arr[i])
 };
+
+
+arr_goops = [6, 986, 466, 420, 69, 666]
+
+for (var i = 0; i < arr_goops.length; i++) {
+	console.log(arr_goops[i]);
+};
